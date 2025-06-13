2fa4b1d72fa1816225edc753753b501baeb9f562
RoboDoig <erskine94@gmail.com>
2025-06-13 17:00:22 +0100

    Schema sandbox prototype

---
+++ Workflows/Extensions/ShuffleArray.cs
@@ -0,0 +1,22 @@
+using Bonsai;
+using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+
+[Combinator]
+[Description("")]
+[WorkflowElementCategory(ElementCategory.Transform)]
+public class ShuffleArray
+{
+    Random rnd = new Random();
+
+    public IObservable<List<double>> Process(IObservable<List<double>> source)
+    {
+        return source.Select(value =>
+        {
+            return value.OrderBy(x => rnd.Next()).ToList();
+        });
+    }
+}
