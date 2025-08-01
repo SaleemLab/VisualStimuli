31daace5750d4e066b240128361df5cbdcd35220
RoboDoig <erskine94@gmail.com>
2025-08-01 16:06:43 +0100

    Trying pydantic schema

---
+++ VisStimMachine/Schemas/state-machine.py
@@ -0,0 +1,42 @@
+import json
+from pathlib import Path
+from typing import Annotated, Literal, Union, List
+from pydantic import BaseModel, Field, RootModel
+import os
+
+class VisualBase(BaseModel):
+    visual_type: str
+
+class Gratings(VisualBase):
+    visual_type: Literal["gratings"]
+    temporal_frequency: float = Field(default=1.0)
+
+class CheckerBoard(VisualBase):
+    visual_type: Literal["checkerboard"]
+    spatial_frequency: float = Field(default=1.0)
+
+class VisualStimulus(RootModel):
+    root: Annotated[Union[Gratings, CheckerBoard], Field(discriminator="visual_type")]
+
+class LogicBase(BaseModel):
+    logic_type: str
+
+class Timer(LogicBase):
+    logic_type: Literal["timer"]
+    due_time: float = Field(default=1.0)
+
+class KeyDown(LogicBase):
+    logic_type: Literal["keydown"]
+    key_down: str = Field(default="A")
+
+class State(BaseModel):
+    alias: str
+    visual: VisualStimulus
+
+class StateMachine(BaseModel):
+    state_definitions: List[State]
+
+if __name__ == "__main__":
+    schema = StateMachine.model_json_schema()
+    Path("Schemas\state-machine.json").write_text(json.dumps(schema, indent=2))
+    os.system("dotnet bonsai.sgen ""Schemas\state-machine.json"" -o Workflows\Extensions --serializer json")
